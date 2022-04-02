fb04e41244bc72526b59f460ce4345749eb40c86
buffman23 <ryancoughlin9@yahoo.com>
2022-04-01 19:53:32 -0700

    Added spikes to Floor Hole Trap. Hole Trap now TPs player out.

---
+++ Assets/Scripts/HoleTrap.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleTrap : MonoBehaviour
+{
+    private BoxCollider _collisionBox;
+    private Transform _respawnTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitReferences();
+
+
+    }
+
+    private void InitReferences()
+    {
+        _collisionBox = GetComponent<BoxCollider>();
+        _respawnTrans = transform.Find("Respawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        
+        GameObject go = collider.gameObject;
+        Debug.Log("Trigger entered: " + go.name);
+
+        Vector3 respawnPosition = _respawnTrans.position + new Vector3(0f, go.transform.lossyScale.y / 2f, 0f);
+
+        CharacterController cc = go.GetComponent<CharacterController>();
+        if (cc != null)
+        {
+            bool wasEnabled = cc.enabled;
+            cc.enabled = false;
+            go.transform.position = respawnPosition;
+            cc.enabled = wasEnabled;
+        }
+        else 
+        {
+            go.transform.position = respawnPosition;
+        }
+    }
+}
