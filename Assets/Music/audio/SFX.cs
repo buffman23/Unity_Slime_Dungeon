1332d46d04bf6d23f457ddb4138c3b13045c3333
Manny <eguinto@sdsu.edu>
2022-04-28 16:37:55 -0700

    Added sound files to Unity, added music to game scene, and main menu scene

---
+++ Assets/Music/audio/SFX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlaying : MonoBehaviour
+{
+    public AudioSource SwordSwing;
+
+    public void PlaySwordSWing()
+    {
+        SwordSwing.Play();
+    }
+}
